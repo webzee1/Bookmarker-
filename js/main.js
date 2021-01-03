6eff97c9756448dbbcbb7bdaa287977c9e226cba
Zaryab Haider <haiderzaryab71@gmail.com>
2021-01-04 01:09:55 +0500

    Validations , Fetching Local storage data & Display code

--- js/main.js
+++ js/main.js
@@ -1,37 +1,117 @@
-//Listen for form submit
+// Listen for form submit
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
-//Save Bookmarker
-function saveBookmark(event){
-  event.preventDefault();
+// Save Bookmark
+function saveBookmark(e){
+  // Get form values
+  var siteName =document.getElementById('siteName').value;
+  var siteUrl =document.getElementById('siteUrl').value;
 
-  //Get form Values
-  var siteName = document.getElementById('siteName').value;
-  var siteUrl = document.getElementById('siteUrl').value;
+  if(!validateForm(siteName, siteUrl)){
+    return false;
+  }
 
   var bookmark = {
-    name : siteName,
-    url :siteUrl
+    name: siteName,
+    url: siteUrl
   }
 
-  //Check Bookmark is null
+  /*
+    // Local Storage Test
+    localStorage.setItem('test', 'Hello World');
+    console.log(localStorage.getItem('test'));
+    localStorage.removeItem('test');
+    console.log(localStorage.getItem('test'));
+  */
+
+  // Test if bookmarks is null
   if(localStorage.getItem('bookmarks') === null){
-    //Init Array
+    // Init array
     var bookmarks = [];
-    // Add to Array
+    // Add to array
     bookmarks.push(bookmark);
     // Set to localStorage
-    localStorage.setItem('bookmarks' , JSON.stringify(bookmarks));
-    // console.log(bookmarks);
-  }
-  else {
-    // Get Bookmark from Localstorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  } else {
+    // Get bookmarks from localStorage
     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-    //Add bookmark to an Array
+    // Add bookmark to array
     bookmarks.push(bookmark);
-    // Re-set back to localstorage
-    localStorage.setItem('bookmarks' , JSON.stringify(bookmarks));
+    // Re-set back to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   }
 
+  // Clear form
+  document.getElementById('myForm').reset();
 
+  // Re-fetch bookmarks
+  fetchBookmarks();
+
+  // Prevent form from submitting
+  e.preventDefault();
 }
+
+// Delete bookmark
+function deleteBookmark(url){
+  // Get bookmarks from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  // Loop through the bookmarks
+  for(var i =0;i < bookmarks.length;i++){
+    if(bookmarks[i].url == url){
+      // Remove from array
+      bookmarks.splice(i, 1);
+    }
+  }
+  // Re-set back to localStorage
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+  // Re-fetch bookmarks
+  fetchBookmarks();
+}
+
+// Fetch bookmarks
+function fetchBookmarks(){
+  // Get bookmarks from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  // Get output id
+  var bookmarksResults = document.getElementById('bookmarksResults');
+
+  // Build output
+  bookmarksResults.innerHTML = '';
+  for(var i = 0; i < bookmarks.length; i++){
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+
+    bookmarksResults.innerHTML += '<div class="well">'+
+                                  '<h3>'+name+
+                                  ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> ' +
+                                  ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
+                                  '</h3>'+
+                                  '</div>';
+  }
+}
+
+// Validate Form
+function validateForm(siteName, siteUrl){
+  if(!siteName || !siteUrl){
+    alert('Please fill in the form');
+    return false;
+  }
+
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+
+  if(!siteUrl.match(regex)){
+    alert('Please use a valid URL');
+    return false;
+  }
+
+  return true;
+}
+
+// function addhttp(url) {
+//   if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+//       url = "http://" + url;
+//   }
+//   return url;
+// }
